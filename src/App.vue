1de0bf902d4f0fe669f35f3383c7808208975440
Ali Mohamed <alimohamed351@outlook.com>
2025-06-20 15:23:45 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Home from './views/Home.vue'
+import Nav from './components/Nav.vue'
+import Footer from './components/Footer.vue'
+
+export default defineComponent({
+  name: 'home',
+  components: {
+    Nav,
+    Footer
+  }
+}) 
+</script>
+
+<template>
+  <div class="app">
+    <Nav />
+
+    <main>
+      <RouterView />
+    </main>
+
+    <!-- <Footer /> -->
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/Products.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>this is the prod page</h1>
+</template>
